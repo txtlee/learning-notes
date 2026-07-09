b4ba6a7fa7bb3a523663a499a59c65fe48b2dc65
txtLee <txtlee@txtLeedeMacBook-Pro.local>
2026-07-09 10:33:34 +0800

    138 随机链表的复制

--- temp.py
+++ temp.py
@@ -1,2 +1,24 @@
-for i in range(5):
-    print(i)
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+# class Solution(object):
+#     def copyRandomList(self, head):
+#         """
+#         :type head: Node
+#         :rtype: Node
+#         """
+#         if not head:
+
+#         node_to_copy_dict = {}
+
+x = ""
+if not x:
+    print("3")
+else:
+    print("2")
